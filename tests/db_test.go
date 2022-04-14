87dc4c37c01765396cf8df4c78cc80229cc650fd
gaixianggeng <gaih@outlook.com>
2022-04-15 00:12:49 +0800

    test forward

--- tests/db_test.go
+++ tests/db_test.go
@@ -1,10 +1,20 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/binary"
 	"os"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func TestWrite(t *testing.T) {
-	os.ReadFile("../data/forward.db")
+	log.SetLevel(log.DebugLevel)
+	c, _ := os.ReadFile("../data/forward.db")
+	log.Debug(len(c))
+	buf := bytes.NewBuffer(c)
+	a := make([]uint64, 21)
+	binary.Read(buf, binary.LittleEndian, &a)
+	log.Debug(a)
 }
